241d85dfc454fe14c6674d7c4b7b1bf1f08624e9
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-06 09:41:04 +0700

    Modified 'confirmation' property

--- libs.js
+++ libs.js
@@ -1,7 +1,36 @@
 LIBS = {
 
 	confirmation: function(args){
-		
+		/*
+		*
+		* Available Parameters :
+		*---------------------------------------
+		* (1) .element -> The mother element for your confirmation. (optional)
+		* (2) .text -> Displayed text
+		* (3) .okAction -> Performing OK button
+		*
+		*/
+
+		var el = (
+			(args !== null
+				&& typeof args === 'object'
+				&& typeof args.element !== 'undefined')
+			? args.element
+			: '#confirmation'
+		);
+
+		$(el + ' .text').html(args.text);
+		$(el + ' .negative').on('click', function(){
+			$(this).closest('.container').slideUp(300, function(){
+				$(this).parent().fadeOut();
+			});
+		});
+
+		$(el + ' .positive').on('click', args.okAction);
+
+		$(el).fadeIn(300, function(){
+			$(this).children().slideDown(150);
+		});
 	}
 
 }
