bf29fa43ede9bfc09642c33d6dac2bdf5c8cd9c2
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-06 09:42:04 +0700

    Call our 'confirmation'

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 index = {
 
 	init: function(){
-		
+		index._open();
+	},
+	_open: function(){
+		$('a').on('click', function(){
+			LIBS.confirmation(
+				{
+					text		: 'Do you really want to delete this file?',
+					okAction	: function(){
+						alert('You clicked OK button');
+					}
+				}
+			);
+		});
 	}
 
 };
 
-jQuery(index.init());
+jQuery(index.init);
